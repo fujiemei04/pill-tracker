ff6f3665a800717f16f43c3256037a1857d8a94d
fujie.mei <fujie.mei@estudiantat.upc.edu>
2023-11-10 19:30:43 +0100

    Initial commit

---
+++ src/main/java/com/pill/pilltracker/PillTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.pill.pilltracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PillTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PillTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pill/pilltracker/PillTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pill.pilltracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PillTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
